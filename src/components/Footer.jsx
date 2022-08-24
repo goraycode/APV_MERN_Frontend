592b58b6e233580edf8b4c3c5f2eab47715d4760
AYGJ <jofre1236@gmail.com>
2022-08-23 19:03:50 -0500

    Change title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="py-10">
-      <p className="py-5 text-center bg bg-black text-white">
+      <p className="py-10 text-center bg bg-black text-white">
         APV- Administrador de pacientes de{" "}
         <span className="text-indigo-500">pacientes</span>
       </p>
